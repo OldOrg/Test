0df4e0a2c262a07eb12ab6cd5451ce6522d5231e
Chris Ollenburg <chris.ollenburg@gmail.com>
2012-01-23 21:19:12 -0500

    Added comment

--- HelloWorld/src/com/ollendev/helloworld/HelloWorldActivity.java
+++ HelloWorld/src/com/ollendev/helloworld/HelloWorldActivity.java
@@ -7,6 +7,7 @@ public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	//This is a comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
